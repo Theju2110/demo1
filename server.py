787229f89edc3c0a3a2de33b41226d020b852db8
Theju2110 <140256307+Theju2110@users.noreply.github.com>
2023-07-23 19:22:43 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import socket
+import select
 
 # Server IP and port
-SERVER_IP = "172.23.0.1"
+SERVER_IP = "192.168.43.205"
 SERVER_PORT = 12345
 
 def main():
@@ -15,31 +16,50 @@ def main():
     server_socket.listen(5)
     print(f"Server listening on {SERVER_IP}:{SERVER_PORT}")
     
+    # List of sockets for select
+    sockets_list = [server_socket]
+    
     while True:
-        # Accept incoming connection
-        client_socket, client_address = server_socket.accept()
-        print(f"Connected to {client_address}")
+        # Use select to wait for incoming data and connected sockets
+        read_sockets, _, _ = select.select(sockets_list, [], [])
         
-        while True:
-            # Receive data from the client
-            data = client_socket.recv(1024).decode()
-            
-            if not data:
-                break
-            
-            print(f"Received from {client_address}: {data}")
-            
-            if data.lower() == "bye":
-                response = "Goodbye"
-                client_socket.send(response.encode())
-                break
-            
-            # Send response to the client
-            response = input("Enter your response: ")
-            client_socket.send(response.encode())
-        
-        client_socket.close()
-        print(f"Connection closed with {client_address}")
+        for socket_item in read_sockets:
+            if socket_item == server_socket:
+                # New client connection
+                client_socket, client_address = server_socket.accept()
+                print(f"Connected to {client_address}")
+                
+                # Add the new client socket to the list
+                sockets_list.append(client_socket)
+            else:
+                # Existing client sending data
+                try:
+                    # Receive data from the client
+                    data = socket_item.recv(1024).decode()
+                    
+                    if not data:
+                        # Client closed the connection
+                        print(f"Connection closed with {client_address}")
+                        socket_item.close()
+                        sockets_list.remove(socket_item)
+                        continue
+                    
+                    print(f"Received from {client_address}: {data}")
+                    
+                    if data.lower() == "bye":
+                        response = "Goodbye"
+                        socket_item.send(response.encode())
+                        socket_item.close()
+                        sockets_list.remove(socket_item)
+                    else:
+                        # Send response to the client
+                        response = input("Enter your response: ")
+                        socket_item.send(response.encode())
+                except Exception as e:
+                    # Client encountered an error, close the connection
+                    print(f"Error with {client_address}: {e}")
+                    socket_item.close()
+                    sockets_list.remove(socket_item)
 
 if __name__ == "__main__":
     main()
